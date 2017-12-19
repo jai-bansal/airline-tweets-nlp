4c7cfd8d680feff43621613465c8b54db3b12b60
jai-bansal <jbans79@gmail.com>
2017-12-18 17:57:32 -0800

    progress on rnn
    - slow progress
    - didn't like any tensorflow rnn scripts in R, so I'll go with 'mxnet'
    - slowly working my way through an example script

--- mxnet_rnn.R
+++ mxnet_rnn.R
@@ -20,6 +20,7 @@ data = data.table(read_csv('Airline-Sentiment-2-w-AA.csv'))
                collapse = ' ')
   
   # Remove unwanted characters from 'text'.
+  # Note that I couldn't remove forward slashes.
   text = gsub('@', '', text)  # '@' signs
   text = gsub('$', '', text)  #  dollar signs
   text = gsub(',', '', text)  # commas
@@ -38,6 +39,10 @@ data = data.table(read_csv('Airline-Sentiment-2-w-AA.csv'))
   text = gsub('\\(', '', text)  # left parentheses
   text = gsub('\\)', '', text)  # right parentheses
   text = gsub('[[:digit:]]+', '', text)   # numbers
+  
+  # Make 'text' lowercase.
+  text = tolower(text)
+  
   text = gsub('virginamerica', '', text)  # airline tag
   text = gsub('americanair', '', text)  # airline tag
   text = gsub('united', '', text)  # airline tag
@@ -47,12 +52,82 @@ data = data.table(read_csv('Airline-Sentiment-2-w-AA.csv'))
   text = trimws(text, 
                 which = 'both')   # trim leading and trailing whitespace
   text = gsub('  ', ' ', text)  # replace double spaces with single spaces
-  
-  # Make 'text' lowercase.
-  text = tolower(text)
+
   
   
+  
+  
+  
+    
+# SET MODEL PARAMETERS ----------------------------------------------------
+
+  # Set model parameters.
+  # Will figure these out over time.
+  batch.size = 32
+  seq.len = 32
+  num.hidden = 16
+  num.embed = 16
+  num.lstm.layer = 1
+  num.round = 1
+  learning.rate= 0.1
+  wd=0.00001
+  clip_gradient=1
+  update.period = 1
   
 
+# CREATE CHARACTER / ID DICTIONARIES ---------------------------------------------
+# This section creates 2 dictionaries.
+# The first is a dictionary where each dictionary name is a character and 
+# the value is an ID.
+# I use the lowercase letters, space, and a holder for other characters.
+# The 2nd dictionary is the reverse: each dictionary name is an ID and each
+# value is a character (for lowercase letters, space, and a holder for unknown characters).
+# Characters are associated with the same ID in both dictionaries.
+# For example, 'a' will be associated with 1 in both dictionaries.
+  
+  # Create word to ID dictionary.
+  # The names of this dictionary are characters.
+  
+    # Create empty dictionary.
+    word_id_dict = list()
+    
+    # Set initial dictionary ID (1).
+    dict_id = 1
+    
+    # Loop through the lowercase letters. Add each one as a dictionary name.
+    # The corresponding value is an increasing ID.
+    for (letter in letters) 
+      
+    {
+      
+      # Add 'letter' to 'word_id_dict' with corresponding and increasing 'dict_id'.
+      word_id_dict[[letter]] = dict_id
+      
+      # Increment 'dict_id' by 1.
+      dict_id = dict_id + 1
+      
+    }
+    
+    # Add entry to 'word_id_dict' for space and increment 'dict_id'.
+    word_id_dict[[' ']] = dict_id
+    dict_id = dict_id + 1
+    
+    # Add entry to 'word_id_dict' for all other characters.
+    word_id_dict[['other']] = dict_id
+    
+  # Create ID to word dictionary.
+  # The names of this dictionary are numeric IDs.
+    
+    # Create empty dictionary.
+    id_word_dict = list()
+    
+    # Loop through the names of 'word_id_dict' (characters).
+    for character in names(word_id_dict)
+    
+      {
+      
+        #
+        
+      }
 
-
+  
