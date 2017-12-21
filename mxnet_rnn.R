eed60377f9480fb400d840c1c7df10e99150964e
jai-bansal <jbans79@gmail.com>
2017-12-20 17:21:58 -0800

    progress on lstm script

--- mxnet_rnn.R
+++ mxnet_rnn.R
@@ -63,24 +63,27 @@ data = data.table(read_csv('Airline-Sentiment-2-w-AA.csv'))
 
   # Set model parameters.
   # Will figure these out over time.
-  batch.size = 32
-  seq.len = 32
-  num.hidden = 16
+  batch.size = 32             # guess: batch size
+  
+  seq.len = 32                # educated guess: length of each string in a batch
+  # Set length of each string in the batch (in characters).
+  batch_length = 10
+  
+  num.hidden = 16             # guess: # of hidden nodes
   num.embed = 16
   num.lstm.layer = 1
   num.round = 1
-  learning.rate= 0.1
-  wd=0.00001
+  learning.rate= 0.1          # guess: RNN learning rate
+  wd = 0.00001
   clip_gradient=1
   update.period = 1
   
 
 # CREATE CHARACTER / ID DICTIONARIES ---------------------------------------------
 # This section creates 2 dictionaries.
-# The first is a dictionary where each dictionary name is a character and 
-# the value is an ID.
+# In the first, each dictionary name is a character and the value is a numeric ID.
 # I use the lowercase letters, space, and a holder for other characters.
-# The 2nd dictionary is the reverse: each dictionary name is an ID and each
+# The 2nd dictionary is the reverse: each dictionary name is a numeric ID and each
 # value is a character (for lowercase letters, space, and a holder for unknown characters).
 # Characters are associated with the same ID in both dictionaries.
 # For example, 'a' will be associated with 1 in both dictionaries.
@@ -122,12 +125,96 @@ data = data.table(read_csv('Airline-Sentiment-2-w-AA.csv'))
     id_word_dict = list()
     
     # Loop through the names of 'word_id_dict' (characters).
-    for character in names(word_id_dict)
+    for (char in names(word_id_dict))
     
       {
       
-        #
+        # Get the value corresponding to the name 'char' in 'word_id_dict'.
+        id = word_id_dict[[char]]
+        
+        # Create an element in 'id_word_dict' with name 'id' and value 'char'.
+        id_word_dict[[id]] = char
         
       }
 
+    
+
+
+# PUT TEXT DATA INTO NUMERIC ARRAY ----------------------------------------
+# This section puts the text data into a numeric array.
+# The number of rows is the length of each string in a batch.
+# The number of columns is the number of strings that can fit into the text.
+# The value can be computed based on the length of each string and the 
+# length of the text.
+
+  # Split 'text' by character.
+  text = strsplit(text, '')[[1]]
+    
+  # Compute the number of full strings that can fit into 'text'.
+  # String length is defined by 'batch' length.
+  total_strings = floor(length(text) / batch_length)
   
+  # Only keep enough characters of 'text' to complete 'total_strings' 
+  # strings of 'batch_length' length.
+  text = text[1:(total_strings* batch_length)]
+  
+  # Create empty array.
+  text_array = array(0, dim = c(batch_length, total_strings))
+  
+  # Fill in 'text_array' with the appropriate integer for each character.
+  
+    # Set initial text index to 1.
+    # I will loop through every character of 'text'.
+    text_index = 1
+  
+    # Loop through each string.
+    for (string in 1:total_strings)
+      
+      {
+      
+        # Print progress.
+        if (string %% 30000 == 0)
+          
+          {
+          
+            print(string)
+          
+          }
+        
+        # Loop each character of each string.
+        for (char in 1:batch_length)
+          
+          {
+          
+            # If the 'text_index' character of 'text' is in 'word_id_dict', 
+            # change the corresponding entry in 'text_array' to the corresponding 
+            # 'word_id_dict' value.
+            if (text[text_index] %in% names(word_id_dict))
+              
+              {
+                
+                # Update corresponding entry of 'text_array'.
+                text_array[char, string] = word_id_dict[[text[text_index]]]
+              
+              }
+          
+            # If the 'text_index' character of 'text is NOT in 'word_id_dict', 
+            # change the corresponding entry in 'text_array' to the value 
+            # corresponding to 'other' in 'word_id_dict'.
+            else 
+              
+              {
+              
+                # Update corresponding entry of 'text_array'.
+                text_array[char, string] = word_id_dict[['other']]
+                
+              }
+          
+            # Increment 'text_index'.
+            text_index = text_index + 1
+          
+          }
+      
+      }
+
+    
