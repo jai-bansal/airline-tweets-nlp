140b3818f666d8e97454b2bb4981ca1c16f40320
jai-bansal <jbans79@gmail.com>
2018-01-17 17:57:48 -0800

    add lda analysis

--- topic_model_lda.R
+++ topic_model_lda.R
@@ -69,15 +69,44 @@ data = data.table(read_csv('Airline-Sentiment-2-w-AA.csv'))
                                                 stopwords = T, 
                                                 stemming = T, 
                                                 minWordLength = 2))
+    
+  # Now, I want to see if I want to keep all of the words.
+  # The criteria below are very heuristic and are based somewhat on this video:
+  # https://www.youtube.com/watch?v=3mHy4OSyRf0
   
-  # The above command apparently results in some rows (documents) with no words!
+    # First, I'll count how many times each word occurs. There are 14623 documents.
+    # Note that a word can occur more than once in a document.
+    word_occurrences = apply(dtm, 2, sum)
+    
+    # Remove words that appear once (and thus only in one document).
+    dtm = dtm[1:nrow(dtm), word_occurrences > 1]
+    
+    # Now, I'll count how many DOCUMENTS a word appears in.
+    doc_occurrences = apply(dtm, 2, function(x) {sum(x != 0)})
+    
+    # The most occurring word appears 4817 times in 3908 documents. It's "flight".
+    
+    # Remove words that appear in more than 5% of articles. This is one of those heuristic metrics.
+    # The video referenced above removes words appearing in more than 10% of articles, but I don't have
+    # that much data so...5% it is.
+    dtm = dtm[1:nrow(dtm), doc_occurrences < as.integer(nrow(dtm) / 20)]
+    
+  # The above work apparently results in some rows (documents) with no words!
   # These need to be removed to proceed.
-  
+    
     # Compute the "sum" of words in each document (row).
     row_sum = apply(dtm, 1, sum)
     
     # Remove empty rows (where 'row_sum == 0') from 'dtm'.
     dtm = dtm[row_sum > 0, ]
+    
+  # Update 'data' to reflect the removed documents.
+    
+    # Save a copy.
+    orig_data = data
+    
+    # Update 'data'.
+    data = data[row_sum > 0]
   
 # SELECT OPTIMAL NUMBER OF TOPICS -----------------------------------------
 # This section selects the optimal number of topics for LDA.
@@ -95,8 +124,7 @@ data = data.table(read_csv('Airline-Sentiment-2-w-AA.csv'))
   # View results.
   # I personally am not sure how helpful this is...
   # Arun2010 should be minimized but appears to decrease monotonically as number of topics increases
-  # Deveaud2014 should be maximized and is maximized at 2 topics, after which it appears to decrease monotically 
-  # in the number of topics.
+  # Griffiths 2004 should be maximized but appears to increase monotonically as the number of topics increases
   FindTopicsNumber_plot(topics_number_results)
   
   # Anyway, I'll just use 5 topics.
@@ -109,5 +137,31 @@ data = data.table(read_csv('Airline-Sentiment-2-w-AA.csv'))
   lda = LDA(x = dtm, 
             k = topics, 
             method = 'Gibbs')
-
+  
+  # View the "top" 20 terms in each topic.
+  terms(lda, 20)
+  
+  # View the breakdown of how many documents fall into each topic.
+  table(topics(lda))
+  
+  # View probabilities of belonging to each topics per document.
+  head(posterior(lda)$topics)
+  
+  # Add topic ID to original 'data'.
+  data$topic_id = topics(lda)
+  
+  # I can now look for correlations between the derived topics and some of the metadata.
+  
+    # Compare derived topic to 'airline_sentiment'.
+    # Most tweets were negative, but...
+    # Topic 4 trends very negative.
+    # Topic 5 seems to be slightly less negative.
+    table(data$topic_id, data$airline_sentiment)
+    
+    # Compare derived topic to 'negativereason' where it exists.
+    # Topic 1 got the majority of 'Damaged Luggage' and 'Lost Luggage'
+    # Topic 2 got the majority of 'Flight Booking Problems'
+    # Topic 3 got the majority of 'Late Flight'
+    # Topic 4 got the majority of 'Customer Service Issue'
+    table(data$topic_id, data$negativereason)
   
